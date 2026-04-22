0230b0aded49985d32a0f1ad5a7631ec7e97871d
vanaigr <vanaigranov@gmail.com>
2026-04-22 12:42:22 -0500

    fix: exclude scripts

--- resume-generator/extension/content.js
+++ resume-generator/extension/content.js
@@ -1,3 +1,5 @@
 (() => {
-    return document.body.textContent
+    const clone = document.cloneNode(true)
+    clone.querySelectorAll('script, style, noscript').forEach(el => el.remove())
+    return clone.body.innerText
 })();
