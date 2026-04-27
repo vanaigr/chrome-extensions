72ea51b215f087deff7be72219c141ebbe8eba94
vanaigr <vanaigranov@gmail.com>
2026-04-27 16:20:25 -0500

    fix: greenhouse already visited

--- already-visited/background.js
+++ already-visited/background.js
@@ -24,6 +24,19 @@ function normalize(url) {
     return url;
   }
 }
+function normalizeGreenhouse(url) {
+    const u = new URL(url);
+    const f = u.searchParams.get('for')
+    const token = u.searchParams.get('token')
+
+    u.search = '';
+    u.hash = '';
+    u.searchParams.set('for', f)
+    u.searchParams.set('token', token)
+
+    return u.toString();
+
+}
 
 chrome.webNavigation.onCompleted.addListener(async (details) => {
   if (details.frameId !== 0) return;
@@ -34,19 +47,29 @@ chrome.webNavigation.onCompleted.addListener(async (details) => {
   const { exclusions = [] } = await chrome.storage.sync.get("exclusions");
   if (isExcluded(url, exclusions)) return;
 
-  const target = normalize(url);
   const results = await chrome.history.search({
-    text: target,
+    text: normalize(url),
     startTime: 0,
     maxResults: 10000,
   });
 
   const times = [];
-  for (const r of results) {
-    if (normalize(r.url) !== target) continue;
-    const visits = await chrome.history.getVisits({ url: r.url });
-    for (const v of visits) times.push(v.visitTime);
-  }
+    if(url.hostname === 'job-boards.greenhouse.io') {
+        const target = normalizeGreenhouse(url)
+        for (const r of results) {
+            if (normalizeGreenhouse(r.url) !== target) continue;
+            const visits = await chrome.history.getVisits({ url: r.url });
+            for (const v of visits) times.push(v.visitTime);
+        }
+    }
+    else {
+        const target = normalize(url)
+        for (const r of results) {
+            if (normalize(r.url) !== target) continue;
+            const visits = await chrome.history.getVisits({ url: r.url });
+            for (const v of visits) times.push(v.visitTime);
+        }
+    }
 
   if (times.length >= 2) {
     times.sort((a, b) => b - a);
